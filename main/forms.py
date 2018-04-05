86208b237f187680b42c3cbd60cdd19e6dc6f85f
jayinnn <jayin920805@gmail.com>
2018-04-05 22:33:20 +0800

    update lots of thing

--- main/forms.py
+++ main/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class NameForm(forms.Form):
  	book_isbn = forms.CharField(label='書籍ISBN', max_length=100)
+
+class LoginForm(forms.Form):
+	username = forms.CharField(label='帳號', max_length=50)
+	password = forms.CharField(label='密碼', max_length=50, widget=forms.PasswordInput)
--- main/views.py
+++ main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.http import HttpResponseRedirect
 from .models import Books
-from .forms import NameForm
+from .forms import NameForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 book_list = []
 # Create your views here.
 class IndexView(generic.ListView):
@@ -13,28 +15,42 @@ class IndexView(generic.ListView):
 		"""Return all books in datebase"""
 		return Books.objects.all()
 
+@login_required(login_url='/login/')
 def rent(request):
+	error = False
 	if request.method == 'POST':
 		form = NameForm(request.POST)
 		if form.is_valid():
-			book = Books.objects.get(book_isbn=request.POST['book_isbn'])
-			book_list.append(book)
-			print(book_list)
-
+			isbn = form.cleaned_data['book_isbn']
+			error = False
+			try:
+				book = Books.objects.get(book_isbn=isbn)
+				book_list.append(book)
+			except:
+				error = True
+				pass
+			form = NameForm()
 	else:
 		form = NameForm()
-	return render(request, 'main/rent.html', {'form':form, 'book_list':book_list})
+	return render(request, 'main/rent.html', {'form':form, 'book_list':book_list, 'error':error})
 
 def detail(request, books_id):
 	book = get_object_or_404(Books, pk=books_id) # use book_id as a url
 	return render(request, 'main/detail.html', {'book': book}) #return a dict contain book's date
 
 
-# def create(request):
-# 	if request.method == 'POST':
-# 		form = ArticleForm(request.POST)
-# 		if form.is_valid():
-# 			new_article = form.save()
-# 			return HttpResponceRedirect('/main/' + str(new_article.pk))
-# 		form = ArticleForm()
-# 		return render(request, 'create_article.html')
+def loginview(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect('/book/')
+
+	else:
+		form = LoginForm()
+	return render(request, 'main/login.html', {'form':form})
+def logoutview(request):
+	logout(request)
+	return HttpResponseRedirect('/book/')
